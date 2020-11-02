ceac3575ca5df048b89ecd66f45c4df8649e6931
Sofii Alcaraz <sofiialcaraz16@gmail.com>
2020-11-02 16:15:57 -0300

    add otracalle

--- src/juego/Calle.java
+++ src/juego/Calle.java
@@ -11,7 +11,7 @@ public class Calle {
 	private double x;
 	private double y;
 	private double bajadaDePantalla;// podria ser deslizar
-	private Auto [] autos;
+	private Auto[] autos;
 
 	public Calle(double altura, double ancho, double x, double y, double bajadaDePantalla) {
 		this.largo = altura;
@@ -20,7 +20,7 @@ public class Calle {
 		this.x = x;
 		this.y = y;
 		this.bajadaDePantalla = bajadaDePantalla;
-		this.autos=new Auto[6];
+		this.autos = new Auto[6];
 	}
 
 	public void mover() {
@@ -34,15 +34,18 @@ public class Calle {
 	public void dibujar(Entorno entorno) {
 		entorno.dibujarRectangulo(x, y, ancho, largo, 0, color);
 	}
-	
-	
+
 	public void agregarAutos(Entorno entorno) {
-		for(int i=0;i<autos.length;i++) {
-			if(autos[i]==null) {
-			autos[i]=new Auto(60, 45, entorno.ancho(), entorno.alto() / 4, 3, true, bajadaDePantalla);
+		double alto = entorno.alto() / 4;
+		int cant = 0;
+		for (int i = 0; i < autos.length; i++) {
+			if (autos[i] == null) {
+				autos[i] = new Auto(60, 45, entorno.ancho(), alto, 3, true, bajadaDePantalla);
+			}
+			if (cant == 3) {
+				alto++;
 			}
 		}
 	}
-	
 
 }
