625eac99c216b436607be6c1f3fdc5468eef747e
Sofii Alcaraz <sofiialcaraz16@gmail.com>
2020-11-02 15:52:30 -0300

    remove pruebaCalle

--- src/juego/Calle.java
+++ src/juego/Calle.java
@@ -11,6 +11,7 @@ public class Calle {
 	private double x;
 	private double y;
 	private double bajadaDePantalla;// podria ser deslizar
+	private Auto [] autos;
 
 	public Calle(double altura, double ancho, double x, double y, double bajadaDePantalla) {
 		this.largo = altura;
@@ -19,6 +20,7 @@ public class Calle {
 		this.x = x;
 		this.y = y;
 		this.bajadaDePantalla = bajadaDePantalla;
+		this.autos=new Auto[6];
 	}
 
 	public void mover() {
@@ -32,6 +34,15 @@ public class Calle {
 	public void dibujar(Entorno entorno) {
 		entorno.dibujarRectangulo(x, y, ancho, largo, 0, color);
 	}
-
+	
+	
+	public void agregarAutos(Entorno entorno) {
+		for(int i=0;i<autos.length;i++) {
+			if(autos[i]==null) {
+			autos[i]=new Auto(60, 45, entorno.ancho(), entorno.alto() / 4, 3, true, bajadaDePantalla);
+			}
+		}
+	}
+	
 
 }
