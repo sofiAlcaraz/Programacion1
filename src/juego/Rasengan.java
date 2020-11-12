e08ac5500c50b4968d2ac7a6d2bad0302a1b4745
Sofii Alcaraz <sofiialcaraz16@gmail.com>
2020-11-12 15:22:06 -0300

    add constructorRasengan

--- src/juego/Rasengan.java
+++ src/juego/Rasengan.java
@@ -1,5 +1,25 @@
 package juego;
 
-public class Rasengan {
+import java.awt.Color;
 
+public class Rasengan {
+		private double y;
+		private double x;
+		private double velosidad;
+		private double sentido;//solo para adelnte o para los costados tambien?
+		private double deslizamiento;
+		private Color color;
+		private int diametro;
+		
+		public Rasengan(double y, double x, double sentido, double deslizamiento) {
+			this.y = y;
+			this.x = x;
+			this.velosidad =3;
+			this.sentido = sentido;
+			this.deslizamiento = deslizamiento;
+			this.color = Color.YELLOW;
+			this.diametro = 2;
+		}
+		
+		
 }
